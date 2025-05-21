59d7629095dd07508a7ce131b1bc1b0ca4f78fe6
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2025-05-21 12:21:50 +0530

    Minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,52 +4,55 @@ import PageNavigator from "./Components/PageNavigator";
 
 function App() {
   const [data, setData] = useState([]);
-  const [currentPage,setCurrentPages] = useState(1);
-  const [currentData,setCurrentData] = useState([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentData, setCurrentData] = useState([]);
 
-
-  let dataPerPages = 4;
-  let totalPages =  Math.ceil(data.length / dataPerPages);
-
-  console.log(totalPages);
-  console.log('currenct' , currentPage);
-
-  let lastIndex = dataPerPages * currentPage; 
-  let fisrtIndex = lastIndex - dataPerPages;
-
-  let content = data.slice(fisrtIndex,lastIndex)
-  console.log(content);
-  
-  
-  useEffect(()=>{
-    setCurrentData(()=>data.slice(fisrtIndex,lastIndex))
-  },[currentPage])
+  const dataPerPage = 4;
+  const totalPages = Math.ceil(data.length / dataPerPage);
 
   useEffect(() => {
-    axios.get(`https://dummyjson.com/products`).then((res) => {
-      console.log(res.data.products);
-      setData(res.data.products)
-    });
-    data && setCurrentData(()=>data.slice(fisrtIndex,lastIndex))
+    // Fetch data once
+    axios.get(`https://dummyjson.com/products`)
+      .then((res) => {
+        setData(res.data.products);
+      });
   }, []);
 
-  return <div className="flex p-4 flex-col items-center justify-center w-full">
+  useEffect(() => {
+    // Recalculate sliced data whenever page or data changes
+    const lastIndex = dataPerPage * currentPage;
+    const firstIndex = lastIndex - dataPerPage;
+    const slicedData = data.slice(firstIndex, lastIndex);
+    setCurrentData(slicedData);
+  }, [currentPage, data]);
+
+  return (
+    <div className="flex p-4 flex-col items-center justify-center w-full">
       <h1 className="font-medium tracking-tight">React Pagination</h1>
       <h2 className="tracking-tight italic">E-commerce Cards</h2>
+      
       <div className="w-full my-8 flex gap-3 justify-center items-center flex-wrap">
-          {
-            currentData.map((product,idx)=>{
-              return (
-                <div className="w-[24%] rounded-lg p-4 bg-zinc-200">
-                  <img src={product.thumbnail} alt="" />
-                  <h6 className="font-medium text-sm">{product.title}</h6>
-                </div>
-              )
-            })
-          }
+        {
+          currentData.map((product) => (
+            <div key={product.id} className="w-[24%] rounded-lg p-4 bg-zinc-200">
+              <img src={product.thumbnail} alt={product.title} />
+              <h6 className="font-medium text-sm">{product.title}</h6>
+            </div>
+          ))
+        }
       </div>
-      {totalPages && <PageNavigator currentPage={currentPage} setCurrentPages={setCurrentPages} totalPages={totalPages} />}
-  </div>;
+
+      {
+        totalPages > 1 && (
+          <PageNavigator
+            currentPage={currentPage}
+            setCurrentPages={setCurrentPage}
+            totalPages={totalPages}
+          />
+        )
+      }
+    </div>
+  );
 }
 
 export default App;
